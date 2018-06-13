d88eb94ceecec1ca85bdd5f485346991ad0b7cbe
seethayya.n <seethayya.n@LP-5CD7465GTX.HCLT.CORP.HCL.IN>
2018-06-13 17:16:34 +0530

    Changed the cross origin url

--- src/main/java/com/okta/developer/demo/CoolCarController.java
+++ src/main/java/com/okta/developer/demo/CoolCarController.java
@@ -1,31 +1,28 @@
 package com.okta.developer.demo;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 class CoolCarController {
-    private CarRepository repository;
+	private CarRepository repository;
 
-    public CoolCarController(CarRepository repository) {
-        this.repository = repository;
-    }
+	public CoolCarController(CarRepository repository) {
+		this.repository = repository;
+	}
 
-    @GetMapping("/cool-cars")
-    @CrossOrigin(origins = "https://still-ravine-79015.herokuapp.com")
-    public Collection<Car> coolCars() {
-        return repository.findAll().stream()
-                .filter(this::isCool)
-                .collect(Collectors.toList());
-    }
+	@GetMapping("/cool-cars")
+	@CrossOrigin(origins = "https://sn-angular-cli-heroku-boiler.herokuapp.com/")
+	public Collection<Car> coolCars() {
+		return repository.findAll().stream().filter(this::isCool).collect(Collectors.toList());
+	}
 
-    private boolean isCool(Car car) {
-        return !car.getName().equals("AMC Gremlin") &&
-                !car.getName().equals("Triumph Stag") &&
-                !car.getName().equals("Ford Pinto") &&
-                !car.getName().equals("Yugo GV");
-    }
+	private boolean isCool(Car car) {
+		return !car.getName().equals("AMC Gremlin") && !car.getName().equals("Triumph Stag")
+				&& !car.getName().equals("Ford Pinto") && !car.getName().equals("Yugo GV");
+	}
 }
