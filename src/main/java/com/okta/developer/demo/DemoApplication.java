b30948489ea70faf5e07a0aa78a2ec5f91a7a4c7
seethayya.n <seethayya.n@LP-5CD7465GTX.HCLT.CORP.HCL.IN>
2018-06-24 08:58:28 +0530

    Added okta oath2 security

--- src/main/java/com/okta/developer/demo/DemoApplication.java
+++ src/main/java/com/okta/developer/demo/DemoApplication.java
@@ -9,10 +9,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+@EnableResourceServer
 @SpringBootApplication
 public class DemoApplication {
 
